1c4d5b24e0edea291f950965e849b8a6af0bc255
Christian Penrod <83792792+Zzzero-hash@users.noreply.github.com>
2025-06-05 16:21:51 -0400

    Update setup_env.sh to use TA-Lib build paths

--- setup_env.sh
+++ setup_env.sh
@@ -15,11 +15,24 @@ source "$VENV_DIR/bin/activate"
 
 pip install --upgrade pip setuptools wheel
 
+# Build TA-Lib C library if not already installed
+if ! ldconfig -p | grep -q libta_lib.so; then
+    wget -q http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+    tar -xzf ta-lib-0.4.0-src.tar.gz
+    (cd ta-lib && ./configure && make && make install)
+    rm -rf ta-lib ta-lib-0.4.0-src.tar.gz
+fi
+
+# Set library paths for TA-Lib Python wheel
+export TA_LIBRARY_PATH=/usr/local/lib
+export TA_INCLUDE_PATH=/usr/local/include
+
 # Install numpy first to avoid compiled wheel issues
 pip install numpy==1.23.5
 
 # Install remaining requirements including Ray with Tune extras
-pip install -r requirements.txt --ignore-installed blinker
+TA_LIBRARY_PATH=/usr/local/lib TA_INCLUDE_PATH=/usr/local/include \
+    pip install -r requirements.txt --ignore-installed blinker
 
 # Install the package in editable mode
 pip install -e .
